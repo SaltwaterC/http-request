fa04f15d2774a2ca6529cb6c512f2ca2535c7af6
Stefan Rusu <saltwaterc@gmail.com>
2011-08-01 17:05:12 +0300

    Renamed the tests.

---
+++ tests/http-response-buffer.js
@@ -0,0 +1,24 @@
+var http = require('../');
+
+var assert = require('assert');
+var common = require('./includes/common.js');
+
+var u = require('url');
+
+var server = common.createFooServer(function () {
+	var url = u.format({
+		protocol: 'http:',
+		hostname: common.options.host,
+		port: common.options.port,
+		path: '/'
+	});
+	http.get({url: url}, function (err, res) {
+		if (err) {
+			throw err;
+		}
+		assert.deepEqual(res.code, 200);
+		assert.deepEqual(res.headers['content-type'], 'text/plain');
+		assert.deepEqual(res.buffer, 'foo');
+		server.close();
+	});
+});
