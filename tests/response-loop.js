15e152a5bd7acda30bd4d4f18f980a820b1d59f8
Stefan Rusu <saltwaterc@gmail.com>
2011-08-02 15:14:10 +0300

    Decoupled the loop test from the common module.

--- tests/response-loop.js
+++ tests/response-loop.js
@@ -1,10 +1,16 @@
 var assert = require('assert');
 var common = require('./includes/common.js');
 
-var http = require('../');
+var hg = require('../');
+var http = require('http');
 
-var server = common.createLoopServer(function () {
-	http.get({url: common.options.url}, function (err, res) {
+var server = http.createServer(function (req, res) {
+	res.writeHead(301, {location: '/'});
+	res.end();
+});
+
+server.listen(common.options.port, common.options.host, function () {
+	hg.get({url: common.options.url}, function (err, res) {
 		assert.ok(err instanceof Error);
 		assert.deepEqual(err.message, 'Redirect loop detected after 10 requests.');
 		assert.deepEqual(err.code, 301);
