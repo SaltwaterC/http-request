40419ec036f20156b831ef2c8b54e61aa025d05c
Stefan Rusu <saltwaterc@gmail.com>
2011-08-06 22:15:49 +0300

    Enabled the http.head() test agains broken DNS names for both HTTP (legacy interface) and HTTPS (DNS hack).

--- tests/request-broken-dns-name.js
+++ tests/request-broken-dns-name.js
@@ -1,22 +1,24 @@
-console.error('Status: Ignored test');
-process.exit(0);
-
 var http = require('../');
 
 var assert = require('assert');
 
 var assertions = function (err, res) {
-	/*
 	assert.ok(err instanceof Error);
-	assert.equal(err.errno, 4);
-	assert.deepEqual(err.code, 'ENOTFOUND');
-	*/
+	assert.equal(err.errno, 8);
+	assert.deepEqual(err.code, 'EBADNAME');
 };
 
+console.error('Status: http.get() workaround not implemented.');
+/*
 http.get({url: 'http://.foo.bar/'}, function (err, res) {
 	assertions(err, res);
 });
+*/
 
 http.head({url: 'http://.foo.bar/'}, function (err, res) {
+	assertions(err, res);
+});
+
+http.head({url: 'https://.foo.bar/'}, function (err, res) {
 	assertions(err, res);
 });
