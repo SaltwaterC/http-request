c6be38e440a4e25ed64bc4794e675dd426eec1eb
SaltwaterC <saltwaterc@gmail.com>
2013-03-15 16:09:15 +0200

    Implemented options.maxBody value checking.

--- tests/request-invalid-maxbody.js
+++ tests/request-invalid-maxbody.js
@@ -8,7 +8,7 @@ var assert = require('assert');
 var common = require('./includes/common.js');
 
 var callbacks = {
-	get: 0
+	exception: 0
 };
 
 var server = http.createServer(function (req, res) {
@@ -20,15 +20,14 @@ server.listen(common.options.port, common.options.host, function () {
 	client.get({
 		url: common.options.url,
 		maxBody: 'foo'
-	}, function (err, res) {
-		callbacks.get++;
-		
-		assert.ok(err instanceof Error);
-		assert.strictEqual(err.message, 'Invalid options.maxBody specification.');
-		assert.strictEqual(err.url, common.options.url);
-		
-		server.close();
-	});
+	}, function (err, res) {});
+});
+
+process.on('uncaughtException', function (err) {
+	callbacks.exception++;
+	assert.ok(err instanceof Error);
+	assert.strictEqual(err.message, 'Invalid options.maxBody specification. Expecting a proper integer value.');
+	server.close();
 });
 
 common.teardown(callbacks);
